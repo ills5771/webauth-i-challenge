0d7f88c7b78cf67f22ee44394404854f1ad02587
ills5771 <ilyayelizarov1@gmail.com>
2019-04-01 13:59:19 -0700

    adds post  enpoint to register user with hashed password and get endpoint to retrieve all users

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const bcrypt = require("bcryptjs");
+
+const db = require("./database/dbConfig.js");
+const Users = require("./users/users-model.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("Welcome to webAuth-I");
+});
+
+server.get("/api/users", (req, res) => {
+  Users.getUsers()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 4);
+  user.password = hash;
+
+  Users.addUser(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 const port = process.env.PORT || 5500;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
