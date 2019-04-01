a9d6be7adbc71785cbd7db194c54a6a75659c2a8
ills5771 <ilyayelizarov1@gmail.com>
2019-04-01 15:21:49 -0700

    adds enpoint to login user

--- index.js
+++ index.js
@@ -39,5 +39,22 @@ server.post("/api/register", (req, res) => {
     });
 });
 
+server.post("/api/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.getBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 const port = process.env.PORT || 5500;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
